c38976a914e272bb290daf768cf89ea7c96c1ba5
arierhiprecious@gmail.com <arierhiprecious@gmail.com>
2023-01-26 10:44:07 -0800

    'latest'

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    'aws': {
+        'key': 'AKIAZZTWQ7HAPRYD3APX',
+        'secret': 'hhUHyhCUY170WRBE2ErAOAUBClZbrK2uFXNShh7z',
+        'ses': {
+            'from': {
+                // replace with actual email address
+                'default': '"arierhiprecious@gmail.com" ', 
+            }, 
+            'region': 'us-east-1' 
+        }
+    }
+};
---
+++ routes/socket.js
@@ -0,0 +1,56 @@
+//import {env} from "../enviroment"
+const { generateAcessToken } = require('../utils/util')
+const uuid = require("uuid")
+const { User } = require("../database/database")
+const jwt = require("jsonwebtoken")
+const AWS = require('aws-sdk')
+const config = require('../config'); // load configurations file
+
+module.exports = async (io) => {
+    io.on('connection', async (socket) => {
+        
+
+
+    })
+
+}
+
+
+
+/*
+User.deleteMany().then(data=>{
+    console.log(data)
+})
+
+Product.deleteMany().then(data=>{
+    console.log(data)
+})
+User.deleteMany().then(data=>{
+    console.log(data)
+})
+Admin.deleteMany().then(data=>{
+    console.log(data)
+})
+Comment.deleteMany().then(data=>{
+    console.log(data)
+})
+Order.deleteMany().then(data=>{
+    console.log(data)
+})
+CommentReply.deleteMany().then(data=>{
+    console.log(data)
+})
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
